cc6f5d731b3d9563b8843317ba01654bd9fc92e8
Yomi Ikuru <captyomjnr@gmail.com>
2023-12-02 20:37:36 +0000

    Format user.lua and remove some comments

--- plugins/user.lua
+++ plugins/user.lua
@@ -24,12 +24,12 @@ return {
         LspReferenceText = { underline = true },
       },
       colors = {
-        cursorline = "#22252b"
+        cursorline = "#22252b",
       },
       options = {
-        cursorline = true
+        cursorline = true,
       },
-    }
+    },
   },
   -- NOTE: Already in AstroNvim
   -- {
@@ -65,9 +65,9 @@ return {
       -- modify the sources part of the options table
       opts.sources = cmp.config.sources {
         { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip",  priority = 750 },
-        { name = "buffer",   priority = 500 },
-        { name = "path",     priority = 250 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
       }
 
       -- return the new table to be used
@@ -89,11 +89,11 @@ return {
 
   -- DISCORD
   {
-    'andweeb/presence.nvim',
+    "andweeb/presence.nvim",
     event = "User AstroLspSetup",
     opts = {
       main_image = "file",
-    }
+    },
   },
 
   -- MARKDOWN
@@ -102,19 +102,19 @@ return {
     ft = { "markdown" },
     cmd = { "Glow" },
   },
-{
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    local actions = require "telescope.actions"
-    opts.mappings = {
-      i = {
-        ["<C-j>"] = actions.cycle_history_next,
-        ["<C-k>"] = actions.cycle_history_prev,
-      },
-    }
-    return opts
-  end
-},
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      local actions = require "telescope.actions"
+      opts.mappings = {
+        i = {
+          ["<C-j>"] = actions.cycle_history_next,
+          ["<C-k>"] = actions.cycle_history_prev,
+        },
+      }
+      return opts
+    end,
+  },
   -- OTHER
   {
     "folke/trouble.nvim",
@@ -130,33 +130,33 @@ return {
         -- refer to the configuration section below
       }
     end,
-    lazy = false
+    lazy = false,
   },
   {
     "rcarriga/nvim-notify",
     config = function(plugin, opts)
-      require("plugins.configs.notify")(plugin, opts)
+      require "plugins.configs.notify"(plugin, opts)
       local notify = require "notify"
-      notify.setup({
-        background_colour = "#000000"
-      })
+      notify.setup {
+        background_colour = "#000000",
+      }
     end,
   },
   {
     "ThePrimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").load_extension('harpoon')
+      require("telescope").load_extension "harpoon"
       require("harpoon").setup()
     end,
-    lazy = false
+    lazy = false,
   },
   { "nvim-treesitter/playground" },
   {
     "romgrk/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup {
-        enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
         throttle = true, -- Throttles plugin updates (may improve performance)
         patterns = {
           -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
@@ -165,14 +165,14 @@ return {
           -- By setting the 'default' entry below, you can control which nodes you want to
           -- appear in the context window.
           default = {
-            'class',
-            'function',
-            'method',
+            "class",
+            "function",
+            "method",
           },
         },
       }
     end,
-    lazy = false
+    lazy = false,
   },
   {
     "RRethy/vim-illuminate",
@@ -180,74 +180,41 @@ return {
     opts = {
       options = {
         delay = 120,
-      }
+      },
     },
     config = function(_, opts) require("illuminate").configure(opts) end,
   },
 
   -- RUST
+  -- If debugger stops working, try this:
+  -- https://stackoverflow.com/questions/77218022/why-is-my-debugger-in-vscode-not-working-with-rust-after-mac-update-to-sonoma-14
   {
     "simrat39/rust-tools.nvim",
     -- event = "User AstroLspSetup",
     -- config = function()
     --   require("rust-tools").setup({
-    --     tools = {
-    --       inlay_hints = {
-    --         parameter_hints_prefix = "  ",
-    --         other_hints_prefix = "  ",
-    --       },
-    --       autoSetHints = true,
-    --       -- hover_with_actions = true,
-    --       runnables = {
-    --         use_telescope = true,
-    --       },
-    --       -- deprecated
-    --       -- hover_actions = {
-    --       --   auto_focus = true,
-    --       -- },
-    --     },
     --     server = {
     --       -- cmd_env = requested_server._default_options.cmd_env,
 
     --       -- on_init = require("lvim.lsp").common_on_init, -- Looks to be lvim specific
     --       on_attach = require("astronvim.utils.lsp").on_attach,
-
-    --       settings = {
-    --         ["rust-analyzer"] = {
-    --           cargo = {
-    --             allFeatures = true,
-    --           },
-    --           checkOnSave = {
-    --             enable = true,
-    --             command = "clippy",
-    --           },
-    --         }
-    --       },
     --     },
-    --     dap = {
-    --       adapter = require('rust-tools.dap').get_codelldb_adapter(
-    --         codelldb_path, liblldb_path)
-    --     }
     --   })
     -- end,
     ft = { "rust", "rs" },
     -- init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "rust_analyzer") end,
     opts = function()
       local adapter
-      local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
-      if success then
-        local package_path = package:get_install_path()
-        local codelldb_path = package_path .. "/codelldb"
-        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
-        local this_os = vim.loop.os_uname().sysname
+      local package = require("mason-registry").get_package "codelldb"
+      local package_path = package:get_install_path()
+      local codelldb_path = package_path .. "/extension/adapter/codelldb"
+      local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+      local this_os = vim.loop.os_uname().sysname
 
-        -- The liblldb extension is .so for linux and .dylib for macOS
-        liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+      -- The liblldb extension is .so for linux and .dylib for macOS
+      liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
 
-        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-      else
-        adapter = require("rust-tools.dap").get_codelldb_adapter()
-      end
+      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
       return {
         tools = {
           inlay_hints = {
@@ -278,7 +245,7 @@ return {
                 enable = true,
                 command = "clippy",
               },
-            }
+            },
           },
         },
         dap = { adapter = adapter },
@@ -295,18 +262,14 @@ return {
   -- LSP
   {
     "j-hui/fidget.nvim",
-    config = function()
-      require("fidget").setup()
-    end,
-    lazy = false
+    config = function() require("fidget").setup() end,
+    lazy = false,
   },
   {
-    'saecki/crates.nvim',
+    "saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
-    config = function()
-      require('crates').setup()
-    end,
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+    config = function() require("crates").setup() end,
   },
 
   -- FIXME:
@@ -317,29 +280,27 @@ return {
     event = { "InsertEnter" },
     opts = {
       suggestion = {
-        enabled = false
+        enabled = false,
       },
-      panel = { enabled = false }
+      panel = { enabled = false },
     },
-    lazy = false
+    lazy = false,
   },
   {
     "zbirenbaum/copilot-cmp",
     after = { "copilot.lua", "nvim-cmp" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-    lazy = false
+    config = function() require("copilot_cmp").setup() end,
+    lazy = false,
   },
 
   -- OLLAMA
   {
     "David-Kunz/gen.nvim",
     opts = {
-      model = "mistral_7b_openorca"
+      model = "mistral_7b_openorca",
     },
-    lazy = false
-  }
+    lazy = false,
+  },
 
   -- NOTE: Likely not needed
   -- TELESCOPE

261eebffab295c0e0eb7e84dbda80b72effb8c33
Yomi Ikuru <captyomjnr@gmail.com>
2023-12-02 20:37:15 +0000

    Add dap.lua

---
+++ plugins/dap.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    -- opts = function(_, opts) return opts end,
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require "plugins.configs.nvim-dap-ui"(plugin, opts)
+
+      -- disable dap events that are created
+      local dap = require "dap"
+      -- dap.listeners.after.event_initialized["dapui_config"] = nil
+      dap.listeners.before.event_terminated["dapui_config"] = nil
+      dap.listeners.before.event_exited["dapui_config"] = nil
+    end,
+  },
+}

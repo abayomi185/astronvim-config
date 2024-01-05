06398790749b034314884cbf6c014f3c534136ba
Yomi Ikuru <captyomjnr@gmail.com>
2024-01-05 02:11:34 +0000

    add: yamlls customTags for AWS template.yml

---
+++ lsp/config/yamlls.lua
@@ -0,0 +1,26 @@
+return {
+  settings = {
+    yaml = {
+      customTags = {
+        "!Ref",
+        "!GetAtt",
+        "!FindInMap sequence",
+        "!FindInMap scalar",
+        "!GetAtt",
+        "!GetAZs",
+        "!Cidr",
+        "!ImportValue",
+        "!Join sequence",
+        "!Select",
+        "!Split",
+        "!Sub sequence",
+        "!Sub scalar",
+        "!And",
+        "!Equals",
+        "!If",
+        "!Not",
+        "!Or",
+      },
+    },
+  },
+}

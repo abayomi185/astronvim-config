bdca474f5d32ead0cd585aba458516e15dec94c3
Yomi Ikuru <captyomjnr@gmail.com>
2024-02-22 00:00:53 +0000

    feat: update dap-ui layout

--- plugins/dap.lua
+++ plugins/dap.lua
@@ -4,7 +4,7 @@ return {
     -- opts = function(_, opts) return opts end,
     config = function(plugin, opts)
       -- run default AstroNvim nvim-dap-ui configuration function
-      require "plugins.configs.nvim-dap-ui"(plugin, opts)
+      require "plugins.configs.nvim-dap-ui" (plugin, opts)
 
       -- disable dap events that are created
       local dap = require "dap"
@@ -12,6 +12,46 @@ return {
       dap.listeners.before.event_terminated["dapui_config"] = nil
       dap.listeners.before.event_exited["dapui_config"] = nil
     end,
+    opts = {
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+            {
+              id = "watches",
+              size = 0.25,
+            },
+          },
+          position = "right",
+          size = 40,
+        },
+        {
+          elements = {
+            {
+              id = "repl",
+              size = 0.5,
+            },
+            {
+              id = "console",
+              size = 0.5,
+            },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
+    },
   },
   {
     "abayomi185/nvim-dap-probe-rs",

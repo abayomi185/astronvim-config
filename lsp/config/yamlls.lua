0fa031ebb51e0867e9e23d5417b8e22ad758b42b
Yomi Ikuru <captyomjnr@gmail.com>
2024-02-27 21:46:57 +0000

    feat: add support for kubernetes yaml with yamlls

--- lsp/config/yamlls.lua
+++ lsp/config/yamlls.lua
@@ -1,6 +1,21 @@
 return {
   settings = {
     yaml = {
+      schemas = {
+        kubernetes = {
+          -- "daemon.{yml,yaml}",
+          -- "manager.{yml,yaml}",
+          -- "restapi.{yml,yaml}",
+          -- "role.{yml,yaml}",
+          -- "role_binding.{yml,yaml}",
+          "*onfigma*.{yml,yaml}",
+          "*ngres*.{yml,yaml}",
+          "*ecre*.{yml,yaml}",
+          "*eployment*.{yml,yaml}",
+          "*ervic*.{yml,yaml}",
+          -- "kubectl-edit*.yaml",
+        },
+      },
       hover = true,
       completion = true,
       validate = { enable = true },

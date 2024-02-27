13367d080fa4b158f78e6305b0942674cbddac51
Yomi Ikuru <captyomjnr@gmail.com>
2024-02-27 21:47:32 +0000

    feat: add scroll binding for telescope preview and add telescope-ui-select plugin

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,22 +1,28 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-live-grep-args.nvim",
-    -- This will not install any breaking changes.
-    -- For major updates, this must be adjusted manually.
-    version = "^1.0.0",
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    lazy = false,
   },
-  opts = function(_, opts)
-    require("telescope").load_extension "live_grep_args"
-    local actions = require "telescope.actions"
-    opts.defaults.mappings = {
-      i = {
-        ["<C-j>"] = actions.cycle_history_next,
-        ["<C-k>"] = actions.cycle_history_prev,
-        ["<PageUp>"] = actions.preview_scrolling_up,
-        ["<PageDown>"] = actions.preview_scrolling_down,
-      },
-    }
-    return opts
-  end,
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+    opts = function(_, opts)
+      require("telescope").load_extension "live_grep_args"
+      local actions = require "telescope.actions"
+      opts.defaults.mappings = {
+        i = {
+          ["<C-j>"] = actions.cycle_history_next,
+          ["<C-k>"] = actions.cycle_history_prev,
+          ["<PageUp>"] = actions.preview_scrolling_up,
+          ["<PageDown>"] = actions.preview_scrolling_down,
+        },
+      }
+      return opts
+    end,
+  },
 }

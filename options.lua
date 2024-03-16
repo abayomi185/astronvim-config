a051b4a356fc0487b2168644d4d2ea095ab3d0e3
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-16 23:28:05 +0000

    style: run stylua on options.lua

--- options.lua
+++ options.lua
@@ -3,22 +3,22 @@ return {
   opt = {
     -- set to true or false etc.
     relativenumber = false, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "yes:1", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
-    cmdheight = 1, -- sets vim.opt.cmdheight
-    updatetime = 100, -- sets vim.opt.updatetime
+    number = true,          -- sets vim.opt.number
+    spell = false,          -- sets vim.opt.spell
+    signcolumn = "yes:1",   -- sets vim.opt.signcolumn to auto
+    wrap = false,           -- sets vim.opt.wrap
+    cmdheight = 1,          -- sets vim.opt.cmdheight
+    updatetime = 100,       -- sets vim.opt.updatetime
   },
   g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = false, -- disable notifications when toggling UI elements
-    resession_enabled = true, -- enable resession at start
+    mapleader = " ",                 -- sets vim.g.mapleader
+    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true,              -- enable completion at start
+    autopairs_enabled = true,        -- enable autopairs at start
+    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    resession_enabled = true,        -- enable resession at start
   },
   o = {
     background = "dark", -- sets vim.o.background

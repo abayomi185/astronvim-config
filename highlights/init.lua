f07fdaaa3d1c63e6847ce7efaa2c5a990f4951be
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-17 00:23:43 +0000

    test: add NvChad telescope theme

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,24 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
-}
+-- return function()
+--   local get_hlgroup = require("astronvim.utils").get_hlgroup
+--   -- get highlights from highlight groups
+--   local normal = get_hlgroup "Normal"
+--   local fg, bg = normal.fg, normal.bg
+--   local bg_alt = get_hlgroup("Visual").bg
+--   local green = get_hlgroup("String").fg
+--   local red = get_hlgroup("Error").fg
+--   -- return a table of highlights for telescope based on colors gotten from highlight groups
+--   return {
+--     TelescopeBorder = { fg = bg_alt, bg = bg },
+--     TelescopeNormal = { bg = bg },
+--     TelescopePreviewBorder = { fg = green, bg = bg },
+--     TelescopePreviewNormal = { bg = bg },
+--     TelescopePreviewTitle = { fg = bg, bg = green },
+--     TelescopePromptBorder = { fg = bg_alt, bg = bg },
+--     TelescopePromptNormal = { fg = fg, bg = bg_alt },
+--     TelescopePromptPrefix = { fg = red, bg = bg_alt },
+--     TelescopePromptTitle = { fg = bg, bg = red },
+--     TelescopeResultsBorder = { fg = green, bg = bg },
+--     TelescopeResultsNormal = { bg = bg },
+--     TelescopeResultsTitle = { fg = bg, bg = bg },
+--   }
+-- end

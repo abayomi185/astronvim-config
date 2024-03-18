43fbf0931d51db2325ac379fbcbf4501cd8b53eb
Yomi Ikuru <yomi.ikuru@auroraer.com>
2024-03-18 17:26:40 +0000

    feat: disable lua_ls formatting. Using stylua

--- init.lua
+++ init.lua
@@ -75,6 +75,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- "sumneko_lua",
         "tsserver",
+        "lua_ls",
       },
       timeout_ms = 2000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function

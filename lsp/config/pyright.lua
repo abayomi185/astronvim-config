f36f38f68e622c35242d45dd714633480c65ac70
Yomi Ikuru <yomi.ikuru@auroraer.com>
2024-03-20 11:12:49 +0000

    feat: add pyright lsp config and set typeCheckinMode to basic

---
+++ lsp/config/pyright.lua
@@ -0,0 +1,22 @@
+return {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        -- autoSearchPaths = true,
+        -- useLibraryCodeForTypes = true,
+        -- diagnosticMode = "workspace",
+        -- typeCheckingMode = "basic",
+        -- autoImportCompletions = true,
+        -- autoSearchPaths = true,
+        -- diagnosticSeverityOverrides = {
+        --   reportUnusedImport = "information",
+        --   reportMissingImports = "information",
+        --   reportUndefinedVariable = "information",
+        --   reportMissingTypeStubs = "information",
+        --   reportMissingModuleSource = "information",
+        -- },
+      },
+    },
+  },
+}

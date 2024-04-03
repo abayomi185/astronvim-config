b24cce8e280079987c5b8bec0e42f250f4e8fe40
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-03 14:53:42 +0100

    test: nvim-dap-ui has issues

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- WARN: This file breaks neovim and causes memory usage to go out of control
 
 ---@type LazySpec
@@ -14,60 +12,59 @@ return {
   },
 
   -- NOTE: nvim-dap-ui
-  {
-    "rcarriga/nvim-dap-ui",
-    -- opts = function(_, opts) return opts end,
-    config = function(plugin, opts)
-      -- run default AstroNvim nvim-dap-ui configuration function
-      require "plugins.configs.nvim-dap-ui"(plugin, opts)
-
-      -- disable dap events that are created
-      local dap = require "dap"
-      -- dap.listeners.after.event_initialized["dapui_config"] = nil
-      dap.listeners.before.event_terminated["dapui_config"] = nil
-      dap.listeners.before.event_exited["dapui_config"] = nil
-    end,
-    opts = {
-      layouts = {
-        {
-          elements = {
-            {
-              id = "scopes",
-              size = 0.25,
-            },
-            {
-              id = "breakpoints",
-              size = 0.25,
-            },
-            {
-              id = "stacks",
-              size = 0.25,
-            },
-            {
-              id = "watches",
-              size = 0.25,
-            },
-          },
-          position = "right",
-          size = 40,
-        },
-        {
-          elements = {
-            {
-              id = "repl",
-              size = 0.5,
-            },
-            {
-              id = "console",
-              size = 0.5,
-            },
-          },
-          position = "bottom",
-          size = 10,
-        },
-      },
-    },
-  },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   config = function(plugin, opts)
+  --     -- run default AstroNvim nvim-dap-ui configuration function
+  --     require "plugins.configs.nvim-dap-ui"(plugin, opts)
+  --
+  --     -- disable dap events that are created
+  --     local dap = require "dap"
+  --     -- dap.listeners.after.event_initialized["dapui_config"] = nil
+  --     dap.listeners.before.event_terminated["dapui_config"] = nil
+  --     dap.listeners.before.event_exited["dapui_config"] = nil
+  --   end,
+  --   opts = {
+  --     layouts = {
+  --       {
+  --         elements = {
+  --           {
+  --             id = "scopes",
+  --             size = 0.25,
+  --           },
+  --           {
+  --             id = "breakpoints",
+  --             size = 0.25,
+  --           },
+  --           {
+  --             id = "stacks",
+  --             size = 0.25,
+  --           },
+  --           {
+  --             id = "watches",
+  --             size = 0.25,
+  --           },
+  --         },
+  --         position = "right",
+  --         size = 40,
+  --       },
+  --       {
+  --         elements = {
+  --           {
+  --             id = "repl",
+  --             size = 0.5,
+  --           },
+  --           {
+  --             id = "console",
+  --             size = 0.5,
+  --           },
+  --         },
+  --         position = "bottom",
+  --         size = 10,
+  --       },
+  --     },
+  --   },
+  -- },
 
   -- NOTE: nvim-dap-probe-rs
   {

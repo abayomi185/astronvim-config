585825f8a6829514549d2bf04c337d3b60fd8a4c
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-03 16:19:03 +0100

    fix: debug config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,5 +1,3 @@
--- WARN: This file breaks neovim and causes memory usage to go out of control
-
 ---@type LazySpec
 return {
   -- NOTE: persistent-breakpoints
@@ -12,59 +10,66 @@ return {
   },
 
   -- NOTE: nvim-dap-ui
-  -- {
-  --   "rcarriga/nvim-dap-ui",
-  --   config = function(plugin, opts)
-  --     -- run default AstroNvim nvim-dap-ui configuration function
-  --     require "plugins.configs.nvim-dap-ui"(plugin, opts)
-  --
-  --     -- disable dap events that are created
-  --     local dap = require "dap"
-  --     -- dap.listeners.after.event_initialized["dapui_config"] = nil
-  --     dap.listeners.before.event_terminated["dapui_config"] = nil
-  --     dap.listeners.before.event_exited["dapui_config"] = nil
-  --   end,
-  --   opts = {
-  --     layouts = {
-  --       {
-  --         elements = {
-  --           {
-  --             id = "scopes",
-  --             size = 0.25,
-  --           },
-  --           {
-  --             id = "breakpoints",
-  --             size = 0.25,
-  --           },
-  --           {
-  --             id = "stacks",
-  --             size = 0.25,
-  --           },
-  --           {
-  --             id = "watches",
-  --             size = 0.25,
-  --           },
-  --         },
-  --         position = "right",
-  --         size = 40,
-  --       },
-  --       {
-  --         elements = {
-  --           {
-  --             id = "repl",
-  --             size = 0.5,
-  --           },
-  --           {
-  --             id = "console",
-  --             size = 0.5,
-  --           },
-  --         },
-  --         position = "bottom",
-  --         size = 10,
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(_, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      local dap, dapui = require "dap", require "dapui"
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+      -- NOTE: disable dap events that are created
+      -- dap.listeners.after.event_initialized["dapui_config"] = nil
+      dap.listeners.before.event_terminated["dapui_config"] = nil
+      dap.listeners.before.event_exited["dapui_config"] = nil
+
+      dapui.setup(opts)
+    end,
+    opts = {
+      floating = {
+        border = "rounded",
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+            {
+              id = "watches",
+              size = 0.25,
+            },
+          },
+          position = "right",
+          size = 40,
+        },
+        {
+          elements = {
+            {
+              id = "repl",
+              size = 0.5,
+            },
+            {
+              id = "console",
+              size = 0.5,
+            },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
+    },
+  },
 
   -- NOTE: nvim-dap-probe-rs
   {

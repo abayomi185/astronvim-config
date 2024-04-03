dd3f6855655f5920c03e11598c9e1e91a4513170
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-03 14:52:48 +0100

    feat: add lang_rust.lua for rust related configs

---
+++ lua/plugins/lang_rust.lua
@@ -0,0 +1,127 @@
+---@type LazySpec
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@param opts AstroLSPOpts
+    opts = {
+      handlers = { rust_analyzer = false }, -- disable setup of `rust_analyzer`
+      ---@diagnostic disable: missing-fields
+      config = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+              },
+              checkOnSave = {
+                command = "clippy",
+              },
+              assist = {
+                importEnforceGranularity = true,
+                importPrefix = "crate",
+              },
+              completion = {
+                postfix = {
+                  enable = false,
+                },
+              },
+              inlayHints = {
+                lifetimeElisionHints = {
+                  enable = true,
+                  useParameterNames = true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = "rust",
+    opts = function()
+      local adapter
+      local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
+      local cfg = require "rustaceanvim.config"
+      if success then
+        local package_path = package:get_install_path()
+        local codelldb_path = package_path .. "/codelldb"
+        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+        local this_os = vim.loop.os_uname().sysname
+
+        -- The path in windows is different
+        if this_os:find "Windows" then
+          codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+          liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+        else
+          -- The liblldb extension is .so for linux and .dylib for macOS
+          liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        end
+        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+      else
+        adapter = cfg.get_codelldb_adapter()
+      end
+
+      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
+
+      return {
+        tools = {
+          inlay_hints = {
+            parameter_hints_prefix = "  ",
+            other_hints_prefix = "  ",
+          },
+          --   autoSetHints = true,
+          --   runnables = {
+          --     use_telescope = true,
+          --   },
+        },
+        server = astrolsp_avail and astrolsp.lsp_opts "rust_analyzer",
+        dap = { adapter = adapter },
+      }
+    end,
+    config = function(_, opts) vim.g.rustaceanvim = require("astrocore").extend_tbl(opts, vim.g.rustaceanvim) end,
+  },
+  {
+    "Saecki/crates.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          CmpSourceCargo = {
+            {
+              event = "BufRead",
+              desc = "Load crates.nvim into Cargo buffers",
+              pattern = "Cargo.toml",
+              callback = function()
+                require("cmp").setup.buffer { sources = { { name = "crates" } } }
+                require "crates"
+              end,
+            },
+          },
+        },
+      },
+    },
+    opts = {
+      src = {
+        cmp = { enabled = true },
+      },
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      local rustaceanvim_avail, rustaceanvim = pcall(require, "rustaceanvim.neotest")
+      if rustaceanvim_avail then table.insert(opts.adapters, rustaceanvim) end
+    end,
+  },
+}

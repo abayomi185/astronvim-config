3af258607fb0c33a3a89fa2c8047012ecd721d5f
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-03 15:29:55 +0100

    feat: add telescope-dap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+local prefix = "<Leader>fd"
 return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
@@ -24,5 +25,42 @@ return {
       }
       return opts
     end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-dap.nvim",
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              [prefix .. "c"] = {
+                "<Cmd>lua require('telescope').extensions.dap.commands()<CR>",
+                desc = "Telescope DAP commands",
+              },
+              [prefix .. "f"] = {
+                "<Cmd>lua require('telescope').extensions.dap.frames()<CR>",
+                desc = "Telescope DAP frames",
+              },
+              [prefix .. "g"] = {
+                "<Cmd>lua require('telescope').extensions.dap.configurations()<CR>",
+                desc = "Telescope DAP configurations",
+              },
+              [prefix .. "l"] = {
+                "<Cmd>lua require('telescope').extensions.dap.list_breakpoints()<CR>",
+                desc = "Telescope DAP list breakpoints",
+              },
+              [prefix .. "v"] = {
+                "<Cmd>lua require('telescope').extensions.dap.variables()<CR>",
+                desc = "Telescope DAP variables",
+              },
+            },
+          },
+        },
+      },
+    },
+    opts = function() require("telescope").load_extension "dap" end,
   },
 }

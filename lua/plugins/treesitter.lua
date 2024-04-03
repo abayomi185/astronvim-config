3a4bad77f769161e591577e2bdcf0563de3cd35f
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-03 15:30:17 +0100

    feat: add treesitter plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,38 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      -- "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        -- "vim",
+        -- add more arguments for adding more treesitter parsers
+      })
+    end,
+  },
+  -- NOTE: Repl highlights
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "LiadOz/nvim-dap-repl-highlights",
+      dependencies = { "mfussenegger/nvim-dap" },
+      opts = {},
+    },
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "dap_repl" })
+      end
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    event = "User AstroFile",
+    opts = {
+      commented = true,
+      enabled = true,
+      enabled_commands = true,
+    },
+  },
 }

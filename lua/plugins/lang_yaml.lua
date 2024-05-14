7f2c255ddfa9d6e62f06ef1a3f3c5da9601c70ae
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-14 10:45:54 +0100

    feat: add cloudformation custom tags to yamlls config

--- lua/plugins/lang_yaml.lua
+++ lua/plugins/lang_yaml.lua
@@ -7,26 +7,30 @@ return {
     ---@param opts AstroLSPOpts
     opts = {
       config = {
-        yaml = {
-          customTags = {
-            "!fn",
-            "!And",
-            "!If",
-            "!Not",
-            "!Equals",
-            "!Or",
-            "!FindInMap sequence",
-            "!Base64",
-            "!Cidr",
-            "!Ref",
-            "!Ref Scalar",
-            "!Sub",
-            "!GetAtt",
-            "!GetAZs",
-            "!ImportValue",
-            "!Select",
-            "!Split",
-            "!Join sequence",
+        yamlls = {
+          settings = {
+            yaml = {
+              customTags = {
+                "!fn",
+                "!And",
+                "!If",
+                "!Not",
+                "!Equals",
+                "!Or",
+                "!FindInMap sequence",
+                "!Base64",
+                "!Cidr",
+                "!Ref",
+                "!Ref Scalar",
+                "!Sub",
+                "!GetAtt",
+                "!GetAZs",
+                "!ImportValue",
+                "!Select",
+                "!Split",
+                "!Join sequence",
+              },
+            },
           },
         },
       },

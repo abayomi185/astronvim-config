29b18a60e394fc2638aae3b9a45fa175912900f5
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-16 21:40:31 +0100

    feat: update auto-dark-mode interval

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -58,6 +58,7 @@ return {
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
+      update_interval = 60 * 1000,
       set_dark_mode = function()
         -- vim.api.nvim_set_option("background", "dark")
         vim.cmd "colorscheme onedark"

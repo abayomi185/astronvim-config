63f0a379d5a93d79b4b7086a3e52056d9308b18e
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-16 21:52:50 +0100

    fix: lua_ls - change nixpkgs to stable 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim (AstroNvim) Dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 

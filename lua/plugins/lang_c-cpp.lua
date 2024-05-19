81b94b148d388bbd19105d2a3c8e647f56fceae2
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-20 00:44:05 +0100

    feat: add plugins for c/cpp

---
+++ lua/plugins/lang_c-cpp.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      config = {
+        clangd = {
+          capabilities = {
+            offsetEncoding = "utf-8",
+          },
+        },
+      },
+    },
+  },
+  {
+    "Civitasv/cmake-tools.nvim",
+    ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    opts = {},
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        autocmds = {
+          clangd_extensions = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require "clangd_extensions"
+                  vim.api.nvim_del_augroup_by_name "clangd_extensions"
+                end
+              end,
+            },
+          },
+          clangd_extension_mappings = {
+            {
+              event = "LspAttach",
+              desc = "Load clangd_extensions with clangd",
+              callback = function(args)
+                if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+                  require("astrocore").set_mappings({
+                    n = {
+                      ["<Leader>lw"] = { "<Cmd>ClangdSwitchSourceHeader<CR>", desc = "Switch source/header file" },
+                    },
+                  }, { buffer = args.buf })
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+}

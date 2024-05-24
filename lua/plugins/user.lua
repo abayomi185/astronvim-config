8cace41a3e832ce2c22542acbe594ba59045751c
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-24 21:35:40 +0100

    feat: enable better-escape

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -50,7 +50,13 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  {
+    "max397574/better-escape.nvim",
+    opts = {
+      mapping = { "jk" },
+    },
+    enabled = true,
+  },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {

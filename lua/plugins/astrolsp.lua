6c336c93fab1128f16dfa0a32bee202802b3931d
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-03 21:28:46 +0100

    feat: disable rust_analyzer lsp config - managed by rustacean.nvim

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -61,7 +61,7 @@ return {
       -- function(server, opts) require("lspconfig")[server].setup(opts) end
 
       -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
+      rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
       -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
     },
     -- Configure buffer local auto commands to add when attaching a language server

6746dcaec2fa3d6e987f08dc36e146f2cff592fd
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-25 11:47:55 +0100

    feat(lsp): add snyk lsp options

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,4 +5,22 @@ return {
     config = function() require("fidget").setup {} end,
     lazy = false,
   },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@param opts AstroLSPOpts
+    opts = {
+      config = {
+        snyk_ls = {
+          init_options = {
+            activateSnykCode = "true",
+            trustedFolders = { "~/projek/*" },
+            -- Tracking
+            enableTelemetry = "false",
+            sendErrorReports = "false",
+          },
+        },
+      },
+    },
+  },
 }

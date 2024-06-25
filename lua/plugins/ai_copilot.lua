e03a515a7c5912a0b908c3916e265f55203d2ff5
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-25 11:48:28 +0100

    feat(cmp): add plugin - cmp_ai for copilot replacement

--- lua/plugins/ai_copilot.lua
+++ lua/plugins/ai_copilot.lua
@@ -1,5 +1,36 @@
+local function get_env_var(command)
+  local handle = io.popen(command)
+  local result = handle:read "*a"
+  handle:close()
+  return result:gsub("%s+$", "") -- trim trailing newline
+end
+
 ---@type LazySpec
 return {
+  {
+    "tzachar/cmp-ai",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      -- local api_key = get_env_var "gpg --decrypt ~/.openai-api-key.gpg 2>/dev/null"
+
+      local cmp_ai = require "cmp_ai.config"
+      cmp_ai:setup {
+        max_lines = 20,
+        provider = "OpenAI",
+        provider_options = {
+          model = "gpt-3.5-turbo",
+        },
+        notify = false,
+        notify_callback = function(msg) vim.notify(msg) end,
+        run_on_every_keystroke = true,
+        ignored_file_types = {
+          -- default is not to ignore
+          -- uncomment to ignore in lua:
+          -- lua = true
+        },
+      }
+    end,
+  },
   {
     "zbirenbaum/copilot.lua",
     cmd = { "Copilot" },

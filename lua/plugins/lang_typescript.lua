86e1bac5ad78da876ca6e80f6e0aaea0d5845237
Yomi Ikuru <captyomjnr@gmail.com>
2024-07-22 17:10:51 +0100

    feat: add plugins for typescript

---
+++ lua/plugins/lang_typescript.lua
@@ -0,0 +1,114 @@
+---@type LazySpec
+return {
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    event = "BufRead package.json",
+    enabled = false,
+  },
+  {
+    "dmmulroy/tsc.nvim",
+    cmd = "TSC",
+    opts = {},
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+      ---@type AstroLSPOpts
+      "AstroNvim/astrolsp",
+      optional = true,
+      ---@diagnostic disable: missing-fields
+      opts = {
+        autocmds = {
+          eslint_fix_on_save = {
+            cond = function(client) return client.name == "eslint" and vim.fn.exists ":EslintFixAll" > 0 end,
+            {
+              event = "BufWritePost",
+              desc = "Fix all eslint errors",
+              callback = function() vim.cmd.EslintFixAll() end,
+            },
+          },
+        },
+        handlers = { tsserver = false }, -- disable tsserver setup, this plugin does it
+        config = {
+          ["typescript-tools"] = { -- enable inlay hints by default for `typescript-tools`
+            settings = {
+              tsserver_file_preferences = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+              expose_as_code_action = {
+                -- "fix_all",
+                "add_missing_imports",
+                "remove_unused",
+                "remove_unused_imports",
+                "organize_imports",
+              },
+              tsserver_max_memory = 3072,
+            },
+          },
+        },
+      },
+    },
+    ft = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    -- get AstroLSP provided options like `on_attach` and `capabilities`
+    opts = function(_, opts)
+      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
+      -- -- New
+      -- opts.server = astrolsp_avail and astrolsp.lsp_opts "typescript-tools"
+      -- opts.server.root_dir = require("lspconfig.util").root_pattern ".git"
+      -- return opts
+      -- Old
+      if astrolsp_avail then return astrolsp.lsp_opts "typescript-tools" end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    config = function()
+      local dap = require "dap"
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+      local js_config = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+      }
+
+      if not dap.configurations.javascript then
+        dap.configurations.javascript = js_config
+      else
+        require("astrocore").extend_tbl(dap.configurations.javascript, js_config)
+      end
+    end,
+  },
+}

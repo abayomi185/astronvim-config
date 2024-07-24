9ea71a5778d2784bffbf1ae404ed72c02e05df3c
Yomi Ikuru <captyomjnr@gmail.com>
2024-07-24 14:19:52 +0100

    feat: update lang_typescript.lua for debugging

--- lua/plugins/lang_typescript.lua
+++ lua/plugins/lang_typescript.lua
@@ -74,26 +74,60 @@ return {
     optional = true,
     config = function()
       local dap = require "dap"
-      dap.adapters["pwa-node"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "node",
-          args = {
-            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-              .. "/js-debug/src/dapDebugServer.js",
-            "${port}",
+      if not dap.adapters["pwa-node"] then
+        dap.adapters["pwa-node"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "node",
+            args = {
+              require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                .. "/js-debug/src/dapDebugServer.js",
+              "${port}",
+            },
+          },
+        }
+      end
+      if not dap.adapters.node then
+        dap.adapters.node = function(cb, config)
+          if config.type == "node" then config.type = "pwa-node" end
+          local pwa_adapter = dap.adapters["pwa-node"]
+          if type(pwa_adapter) == "function" then
+            pwa_adapter(cb, config)
+          else
+            cb(pwa_adapter)
+          end
+        end
+      end
+
+      local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+      local js_config = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file (Typescript)",
+          cwd = "${workspaceFolder}",
+          runtimeArgs = { "--loader=ts-node/esm" },
+          program = "${file}",
+          runtimeExecutable = "node",
+          sourceMaps = true,
+          protocol = "inspector",
+          outFiles = { "${workspaceFolder}/**/**/*", "!**/node_modules/**" },
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
+          resolveSourceMapLocations = {
+            "${workspaceFolder}/**",
+            "!**/node_modules/**",
           },
         },
-      }
-      local js_config = {
         {
           type = "pwa-node",
           request = "launch",
           name = "Launch file",
           program = "${file}",
           cwd = "${workspaceFolder}",
+          sourceMaps = true,
+          protocol = "inspector",
         },
         {
           type = "pwa-node",
@@ -104,11 +138,13 @@ return {
         },
       }
 
-      if not dap.configurations.javascript then
-        dap.configurations.javascript = js_config
-      else
-        require("astrocore").extend_tbl(dap.configurations.javascript, js_config)
+      for _, language in ipairs(js_filetypes) do
+        if not dap.configurations[language] then dap.configurations[language] = js_config end
       end
+
+      local vscode_filetypes = require("dap.ext.vscode").type_to_filetypes
+      vscode_filetypes["node"] = js_filetypes
+      vscode_filetypes["pwa-node"] = js_filetypes
     end,
   },
 }

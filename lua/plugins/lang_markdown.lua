8b5bf7ff96ae071ff950066bd9176ca0c7daffc4
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-01 19:07:33 +0100

    feat: add markdown.nvim for beautiful markdown viewing right in neovim

--- lua/plugins/lang_markdown.lua
+++ lua/plugins/lang_markdown.lua
@@ -7,4 +7,15 @@ return {
     init = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
   },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      -- "echasnovski/mini.nvim", -- if you use the mini.nvim suite
+      -- 'echasnovski/mini.icons' -- if you use standalone mini plugins
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function() require("render-markdown").setup {} end,
+  },
 }

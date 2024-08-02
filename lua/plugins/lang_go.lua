b937dfe77719dd91044c5fc6492142de511de010
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-02 17:07:25 +0100

    feat: add lang_go.lua

---
+++ lua/plugins/lang_go.lua
@@ -0,0 +1,108 @@
+return {
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable-next-line: missing-fields
+      config = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                ST1003 = true,
+                fieldalignment = false,
+                fillreturns = true,
+                nilness = true,
+                nonewvars = true,
+                shadow = true,
+                undeclaredname = true,
+                unreachable = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              codelenses = {
+                gc_details = true, -- Show a code lens toggling the display of gc's choices.
+                generate = true, -- show the `go generate` lens.
+                regenerate_cgo = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              buildFlags = { "-tags", "integration" },
+              completeUnimported = true,
+              diagnosticsDelay = "500ms",
+              matcher = "Fuzzy",
+              semanticTokens = true,
+              staticcheck = true,
+              symbolMatcher = "fuzzy",
+              usePlaceholders = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- {
+  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(
+  --       opts.ensure_installed,
+  --       { "delve", "gopls", "gomodifytags", "gotests", "iferr", "impl" }
+  --     )
+  --   end,
+  -- },
+  -- {
+  --   "leoluz/nvim-dap-go",
+  --   ft = "go",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     {
+  --       "jay-babu/mason-nvim-dap.nvim",
+  --       optional = true,
+  --       opts = function(_, opts)
+  --         opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "delve" })
+  --       end,
+  --     },
+  --   },
+  --   opts = {},
+  -- },
+  -- {
+  --   "olexsmir/gopher.nvim",
+  --   ft = "go",
+  --   build = function()
+  --     if not require("lazy.core.config").spec.plugins["mason.nvim"] then
+  --       vim.print "Installing go dependencies..."
+  --       vim.cmd.GoInstallDeps()
+  --     end
+  --   end,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     { "williamboman/mason.nvim", optional = true }, -- by default use Mason for go dependencies
+  --   },
+  --   opts = {},
+  -- },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "nvim-neotest/neotest-go" },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-go"(require("astrocore").plugin_opts "neotest-go"))
+    end,
+  },
+}

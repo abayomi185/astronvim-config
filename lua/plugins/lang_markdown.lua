0d1e97f690d3042d47e54c20eec16dbcd386a63d
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-05 10:58:02 +0100

    feat(markdown): remove old markdown preview plugin

--- lua/plugins/lang_markdown.lua
+++ lua/plugins/lang_markdown.lua
@@ -1,13 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
-  },
-  {
     "MeanderingProgrammer/markdown.nvim",
     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
     dependencies = {

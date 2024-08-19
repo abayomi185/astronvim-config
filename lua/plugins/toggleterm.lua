f37c4387ddfefaa6629a33cb12451ca9ba8dddfe
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-19 22:34:14 +0100

    feat(toggleterm): update config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,37 +2,40 @@ local prefix = "<Leader>T"
 return {
   "akinsho/toggleterm.nvim",
   cmd = { "ToggleTerm", "TermExec" },
-  dependencies = {
+  specs = {
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
         local astro = require "astrocore"
         maps.n[prefix] = vim.tbl_get(opts, "_map_sections", "t")
-
         maps.n[prefix] = { desc = " Terminal" }
-        if vim.fn.executable "lazygit" == 1 then
+        if vim.fn.executable "git" == 1 and vim.fn.executable "lazygit" == 1 then
           maps.n["<Leader>g"] = vim.tbl_get(opts, "_map_sections", "g")
-          maps.n["<Leader>gg"] = {
-            function()
+          local lazygit = {
+            callback = function()
               local worktree = astro.file_worktree()
               local flags = worktree and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir)
                 or ""
-              astro.toggle_term_cmd("lazygit " .. flags)
+              astro.toggle_term_cmd { cmd = "lazygit " .. flags, direction = "float" }
             end,
             desc = "ToggleTerm lazygit",
           }
-          maps.n[prefix .. "l"] = maps.n["<Leader>gg"]
+          maps.n["<Leader>gg"] = { lazygit.callback, desc = lazygit.desc }
+          maps.n[prefix .. "l"] = { lazygit.callback, desc = lazygit.desc }
         end
         if vim.fn.executable "node" == 1 then
           maps.n[prefix .. "n"] = { function() astro.toggle_term_cmd "node" end, desc = "ToggleTerm node" }
         end
         local gdu = vim.fn.has "mac" == 1 and "gdu-go" or "gdu"
+        if vim.fn.has "win32" == 1 and vim.fn.executable(gdu) ~= 1 then gdu = "gdu_windows_amd64.exe" end
         if vim.fn.executable(gdu) == 1 then
-          maps.n[prefix .. "u"] = { function() astro.toggle_term_cmd(gdu) end, desc = "ToggleTerm gdu" }
+          maps.n[prefix .. "u"] =
+            { function() astro.toggle_term_cmd { cmd = gdu, direction = "float" } end, desc = "ToggleTerm gdu" }
         end
         if vim.fn.executable "btm" == 1 then
-          maps.n[prefix .. "t"] = { function() astro.toggle_term_cmd "btm" end, desc = "ToggleTerm btm" }
+          maps.n[prefix .. "t"] =
+            { function() astro.toggle_term_cmd { cmd = "btm", direction = "float" } end, desc = "ToggleTerm btm" }
         end
         local python = vim.fn.executable "python" == 1 and "python" or vim.fn.executable "python3" == 1 and "python3"
         if python then
@@ -43,11 +46,11 @@ return {
           { "<Cmd>ToggleTerm size=10 direction=horizontal<CR>", desc = "ToggleTerm horizontal split" }
         maps.n[prefix .. "v"] = { "<Cmd>ToggleTerm size=80 direction=vertical<CR>", desc = "ToggleTerm vertical split" }
         maps.n["<F7>"] = { '<Cmd>execute v:count . "ToggleTerm"<CR>', desc = "Toggle terminal" }
-        maps.t["<F7>"] = { "<Cmd>ToggleTerm<CR>", desc = maps.n["<F7>"].desc }
-        maps.i["<F7>"] = { "<Esc>" .. maps.t["<F7>"][1], desc = maps.n["<F7>"].desc }
-        maps.n["<C-'>"] = maps.n["<F7>"] -- requires terminal that supports binding <C-'>
-        maps.t["<C-'>"] = maps.t["<F7>"] -- requires terminal that supports binding <C-'>
-        maps.i["<C-'>"] = maps.i["<F7>"] -- requires terminal that supports binding <C-'>
+        maps.t["<F7>"] = { "<Cmd>ToggleTerm<CR>", desc = "Toggle terminal" }
+        maps.i["<F7>"] = { "<Esc><Cmd>ToggleTerm<CR>", desc = "Toggle terminal" }
+        maps.n["<C-'>"] = { '<Cmd>execute v:count . "ToggleTerm direction=float"<CR>', desc = "Toggle terminal" } -- requires terminal that supports binding <C-'>
+        maps.t["<C-'>"] = { "<Cmd>ToggleTerm<CR>", desc = "Toggle terminal" } -- requires terminal that supports binding <C-'>
+        maps.i["<C-'>"] = { "<Esc><Cmd>ToggleTerm direction=float<CR>", desc = "Toggle terminal" } -- requires terminal that supports binding <C-'>
       end,
     },
   },
@@ -68,7 +71,7 @@ return {
       vim.opt_local.foldcolumn = "0"
       vim.opt_local.signcolumn = "no"
       if t.hidden then
-        local toggle = function() t:toggle() end
+        local function toggle() t:toggle() end
         vim.keymap.set({ "n", "t", "i" }, "<C-'>", toggle, { desc = "Toggle terminal", buffer = t.bufnr })
         vim.keymap.set({ "n", "t", "i" }, "<F7>", toggle, { desc = "Toggle terminal", buffer = t.bufnr })
       end

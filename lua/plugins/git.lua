94e8cbefdfbc90d3a1c022475c6873566fb84f38
Yomi Ikuru <captyomjnr@gmail.com>
2024-09-20 15:20:12 +0100

    feat(git): add plugin config for gitsigns and disable open git diff mapping
    Issue here is that the git signs plugin is initialised on the User
    AstroGitFile event which I'm certain is triggered after loading the
    custom mappings, so my mapping for diffview is overriden. I could
    declare the diffview plugin after this with the same event and add my
    mappings there, that should resolve this.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,4 +10,74 @@ return {
     optional = true,
     opts = { integrations = { diffview = true } },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = vim.fn.executable "git" == 1,
+    event = "User AstroGitFile",
+    -- TODO: Consider pushing this pattern upstream for this plugin
+    -- It should fix the clash behaviour with diffview mappings defined in the global mappings
+    -- dependencies = {
+    --   "AstroNvim/astrocore",
+    --   ---@type AstroCoreOpts
+    --   opts = {
+    --     mappings = {
+    --       n = {
+    --         ["<Leader>fu"] = {
+    --           "<cmd>UndotreeToggle<CR>",
+    --           desc = "Find undotree",
+    --         },
+    --       },
+    --     },
+    --   },
+    -- },
+    opts = function()
+      local get_icon = require("astroui").get_icon
+      return {
+        signs = {
+          add = { text = get_icon "GitSign" },
+          change = { text = get_icon "GitSign" },
+          delete = { text = get_icon "GitSign" },
+          topdelete = { text = get_icon "GitSign" },
+          changedelete = { text = get_icon "GitSign" },
+          untracked = { text = get_icon "GitSign" },
+        },
+        on_attach = function(bufnr)
+          local astrocore = require "astrocore"
+          local prefix, maps = "<Leader>g", astrocore.empty_map_table()
+          for _, mode in ipairs { "n", "v" } do
+            maps[mode][prefix] = { desc = get_icon("Git", 1, true) .. "Git" }
+          end
+
+          maps.n[prefix .. "l"] = { function() require("gitsigns").blame_line() end, desc = "View Git blame" }
+          maps.n[prefix .. "L"] =
+            { function() require("gitsigns").blame_line { full = true } end, desc = "View full Git blame" }
+          maps.n[prefix .. "p"] =
+            { function() require("gitsigns").preview_hunk_inline() end, desc = "Preview Git hunk" }
+          maps.n[prefix .. "r"] = { function() require("gitsigns").reset_hunk() end, desc = "Reset Git hunk" }
+          maps.v[prefix .. "r"] = {
+            function() require("gitsigns").reset_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+            desc = "Reset Git hunk",
+          }
+          maps.n[prefix .. "R"] = { function() require("gitsigns").reset_buffer() end, desc = "Reset Git buffer" }
+          maps.n[prefix .. "s"] = { function() require("gitsigns").stage_hunk() end, desc = "Stage Git hunk" }
+          maps.v[prefix .. "s"] = {
+            function() require("gitsigns").stage_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+            desc = "Stage Git hunk",
+          }
+          maps.n[prefix .. "S"] = { function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" }
+          maps.n[prefix .. "u"] = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" }
+          -- maps.n[prefix .. "d"] = { function() require("gitsigns").diffthis() end, desc = "View Git diff" }
+
+          maps.n["]g"] = { function() require("gitsigns").next_hunk() end, desc = "Next Git hunk" }
+          maps.n["[g"] = { function() require("gitsigns").prev_hunk() end, desc = "Previous Git hunk" }
+          for _, mode in ipairs { "o", "x" } do
+            maps[mode]["ig"] = { ":<C-U>Gitsigns select_hunk<CR>", desc = "inside Git hunk" }
+          end
+
+          astrocore.set_mappings(maps, { buffer = bufnr })
+        end,
+        worktrees = require("astrocore").config.git_worktrees,
+      }
+    end,
+  },
 }

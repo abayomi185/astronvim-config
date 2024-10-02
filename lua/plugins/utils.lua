c364c4b40292d2be3acfb2d4ab31e23f692d2abd
Yomi Ikuru <captyomjnr@gmail.com>
2024-10-02 16:05:25 +0100

    feat: add file.lua for file/dir related plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -27,29 +27,6 @@ return {
     lazy = false,
   },
 
-  -- NOTE: neo-tree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function(_, opts)
-      opts.window = {
-        position = "right",
-        width = 30,
-        mappings = {
-          ["<space>"] = false, -- disable space until we figure out which-key disabling
-          ["[b"] = "prev_source",
-          ["]b"] = "next_source",
-          o = "open",
-          O = "system_open",
-          h = "parent_or_close",
-          l = "child_or_open",
-          Y = "copy_selector",
-        },
-      }
-      -- return opts
-      require("neo-tree").setup(opts)
-    end,
-  },
-
   -- NOTE: vim-illuminate
   {
     "RRethy/vim-illuminate",

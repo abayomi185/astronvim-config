f113017335122643881b19c42e43e7f17be6b062
Yomi Ikuru <captyomjnr@gmail.com>
2024-11-01 13:15:25 +0000

    feat(yaml): add schemaStore and support for kubernetes

--- lua/plugins/lang_yaml.lua
+++ lua/plugins/lang_yaml.lua
@@ -1,6 +1,57 @@
--- Cloudformation custom tags for yaml_ls
 ---@type LazySpec
 return {
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    dependencies = {
+      {
+        "AstroNvim/astrolsp",
+        optional = true,
+        ---@type AstroLSPOpts
+        opts = {
+          ---@diagnostic disable: missing-fields
+          config = {
+            yamlls = {
+              on_new_config = function(config)
+                config.settings.yaml.schemas = vim.tbl_deep_extend(
+                  "force",
+                  config.settings.yaml.schemas or {},
+                  require("schemastore").yaml.schemas()
+                )
+              end,
+              settings = {
+                yaml = {
+                  schemaStore = { enable = false, url = "" },
+                  schemas = {
+                    kubernetes = {
+                      "namespace.yaml",
+                      "deployment.yaml",
+                      "service.yaml",
+                      "ingress.yaml",
+                      "configmap.yaml",
+                      "secret.yaml",
+                      "persistentvolume.yaml",
+                      "persistentvolumeclaim.yaml",
+                      "statefulset.yaml",
+                      "daemonset.yaml",
+                      "job.yaml",
+                      "cronjob.yaml",
+                      "role.yaml",
+                      "rolebinding.yaml",
+                      "clusterrole.yaml",
+                      "clusterrolebinding.yaml",
+                      "serviceaccount.yaml",
+                      "networkpolicy.yaml",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   {
     "AstroNvim/astrolsp",
     optional = true,
@@ -8,6 +59,7 @@ return {
     opts = {
       config = {
         yaml = {
+          -- Cloudformation custom tags for yaml_ls
           customTags = {
             "!fn",
             "!And",

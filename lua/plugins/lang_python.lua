236ff95b6f8d78026950131f10b946942163efd7
Yomi Ikuru <captyomjnr@gmail.com>
2024-11-13 00:37:41 +0000

    feat(python): add neotest config

--- lua/plugins/lang_python.lua
+++ lua/plugins/lang_python.lua
@@ -27,4 +27,26 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+      opts = {
+        is_test_file = function(file_path)
+          -- Support all python file names
+          if vim.endswith(file_path, ".py") then
+            return true
+          else
+            return false
+          end
+        end,
+      },
+      config = function() end,
+    },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-python"(require("astrocore").plugin_opts "neotest-python"))
+    end,
+  },
 }

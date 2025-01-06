6b97094e0b80d6a07665aa927f2dd4e1c0b5b9ea
Yomi Ikuru <captyomjnr@gmail.com>
2025-01-06 23:13:29 +0000

    feat: migrate from nvim-cmp to blink.cmp

--- lua/plugins/ai_copilot.lua
+++ lua/plugins/ai_copilot.lua
@@ -77,10 +77,4 @@ return {
     },
     lazy = false,
   },
-  {
-    "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua", "nvim-cmp" },
-    config = function() require("copilot_cmp").setup() end,
-    lazy = false,
-  },
 }

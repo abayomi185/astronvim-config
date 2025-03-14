e2dfd527d4e4302be2de53b1bdedb8427e47d698
Yomi Ikuru <captyomjnr@gmail.com>
2025-03-14 15:50:57 +0000

    feat(llm): change default llm model for codecompanion copilot

--- lua/plugins/ai_llm.lua
+++ lua/plugins/ai_llm.lua
@@ -43,9 +43,18 @@ return {
           -- Default adapter is openai
           chat = { adapter = "copilot" },
           inline = { adapter = "copilot" },
+          agent = { adapter = "copilot" },
         },
         adapters = {
-          copilot = function() return require("codecompanion.adapters").extend("copilot", {}) end,
+          copilot = function()
+            return require("codecompanion.adapters").extend("copilot", {
+              schema = {
+                model = {
+                  default = "o3-mini",
+                },
+              },
+            })
+          end,
         },
         display = {
           chat = {

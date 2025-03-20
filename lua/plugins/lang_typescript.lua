530d9f4894e2807deed4b0293ac625b406e71ad7
Yomi Ikuru <captyomjnr@gmail.com>
2025-03-20 10:06:07 +0000

    chore(typescript): remove unmaintained typescript-tools lsp

--- lua/plugins/lang_typescript.lua
+++ lua/plugins/lang_typescript.lua
@@ -84,64 +84,6 @@ return {
     },
     config = function(_, opts) require("vtsls").config(opts) end,
   },
-  -- {
-  --   "pmizio/typescript-tools.nvim",
-  --   enabled = false,
-  --   dependencies = {
-  --     ---@type AstroLSPOpts
-  --     "AstroNvim/astrolsp",
-  --     optional = true,
-  --     ---@diagnostic disable: missing-fields
-  --     opts = {
-  --       autocmds = {
-  --         eslint_fix_on_save = {
-  --           cond = function(client) return client.name == "eslint" and vim.fn.exists ":EslintFixAll" > 0 end,
-  --           {
-  --             event = "BufWritePost",
-  --             desc = "Fix all eslint errors",
-  --             callback = function() vim.cmd.EslintFixAll() end,
-  --           },
-  --         },
-  --       },
-  --       handlers = { tsserver = false, ts_ls = false }, -- disable tsserver setup, this plugin does it
-  --       config = {
-  --         ["typescript-tools"] = { -- enable inlay hints by default for `typescript-tools`
-  --           settings = {
-  --             tsserver_file_preferences = {
-  --               includeInlayParameterNameHints = "all",
-  --               includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  --               includeInlayFunctionParameterTypeHints = true,
-  --               includeInlayVariableTypeHints = true,
-  --               includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-  --               includeInlayPropertyDeclarationTypeHints = true,
-  --               includeInlayFunctionLikeReturnTypeHints = true,
-  --               includeInlayEnumMemberValueHints = true,
-  --             },
-  --             expose_as_code_action = {
-  --               -- "fix_all",
-  --               "add_missing_imports",
-  --               "remove_unused",
-  --               "remove_unused_imports",
-  --               "organize_imports",
-  --             },
-  --             tsserver_max_memory = 3072,
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  --   ft = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  --   -- get AstroLSP provided options like `on_attach` and `capabilities`
-  --   opts = function(_, opts)
-  --     local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-  --     -- -- New
-  --     -- opts.server = astrolsp_avail and astrolsp.lsp_opts "typescript-tools"
-  --     -- opts.server.root_dir = require("lspconfig.util").root_pattern ".git"
-  --     -- return opts
-  --     -- Old
-  --     if astrolsp_avail then return astrolsp.lsp_opts "typescript-tools" end
-  --   end,
-  -- },
   {
     "mfussenegger/nvim-dap",
     optional = true,

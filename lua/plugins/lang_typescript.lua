1e1d726443e2a5cf6e8a06f09fc0d38444b4f0cc
Yomi Ikuru <captyomjnr@gmail.com>
2025-04-11 16:09:35 +0000

    feat(typescript): add mini icons for typescript related files

--- lua/plugins/lang_typescript.lua
+++ lua/plugins/lang_typescript.lua
@@ -13,6 +13,23 @@ return {
     opts = {},
   },
   {
+    "echasnovski/mini.icons",
+    optional = true,
+    opts = {
+      file = {
+        [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+        [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+        [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+        ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+        ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+    },
+  },
+  {
     "AstroNvim/astrolsp",
     optional = true,
     opts = {
@@ -94,6 +111,7 @@ return {
             },
             vtsls = {
               enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
             },
           },
         },

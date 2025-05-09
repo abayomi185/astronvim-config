9d31f15155267e739908bb373c2eaabde693a1f9
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-09 14:45:15 +0000

    fix(mcp): mcphub and codecompanion integration

--- lua/plugins/mcp.lua
+++ lua/plugins/mcp.lua
@@ -33,24 +33,21 @@ return {
       },
     },
     specs = {
-      -- {
-      --   "olimorris/codecompanion.nvim",
-      --   opts = {
-      --     strategies = {
-      --       chat = {
-      --         tools = {
-      --           ["mcp"] = {
-      --             callback = require "mcphub.extensions.codecompanion",
-      --             description = "Call tools and resources from the MCP Servers",
-      --             opts = {
-      --               user_approval = true,
-      --             },
-      --           },
-      --         },
-      --       },
-      --     },
-      --   },
-      -- },
+      {
+        "olimorris/codecompanion.nvim",
+        opts = {
+          extensions = {
+            mcphub = {
+              callback = "mcphub.extensions.codecompanion",
+              opts = {
+                show_result_in_chat = true, -- Show mcp tool results in chat
+                make_vars = true, -- Convert resources to #variables
+                make_slash_commands = true, -- Add prompts as /slash commands
+              },
+            },
+          },
+        },
+      },
     },
   },
 }

2f4a3a6a9348b9792e8c32b5c72db04d2c4389f1
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-12 14:39:08 +0100

    feat(sql): migrate from sqlfluff to sqruff

--- lua/plugins/lang_sql.lua
+++ lua/plugins/lang_sql.lua
@@ -4,13 +4,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        sql = { "sqlfluff" },
-      },
-      formatters = {
-        sqlfluff = {
-          args = { "fix", "--dialect=ansi", "-" },
-          require_cwd = false,
-        },
+        sql = { "sqruff" },
       },
     },
   },
@@ -18,7 +12,7 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
-        sql = { "sqlfluff" },
+        sql = { "sqruff" },
       },
     },
   },

101e69d68fadf90e5aee4ea6dbc7329258e5824f
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-30 16:27:20 +0100

    fix(core): large buffer size function

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -10,7 +10,9 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      large_buf = {
+        enabled = function(bufnr, config) return config.lines > 10000 or config.size > 1024 * 500 end,
+      }, -- set per buffer limit for disabling features like treesitter
       autopairs = true, -- enable autopairs at start
       cmp = true, -- enable completion at start
       diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)

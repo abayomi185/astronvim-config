4326df7eae8df8d1e4d38ac345e4b16195e4f716
Yomi Ikuru <captyomjnr@gmail.com>
2025-07-29 00:25:08 +0100

    feat: move single commands.lua definition into astrocore config

--- lua/plugins/lang_markdown.lua
+++ lua/plugins/lang_markdown.lua
@@ -4,6 +4,19 @@ return {
   {
     "AstroNvim/astrocore",
     opts = function(_, opts)
+      opts.commands = {
+        AddMarkdownHeader = {
+          function()
+            local date = tostring(os.date "### %Y-%m-%d")
+            local cursor = vim.api.nvim_win_get_cursor(0)
+            local row = cursor[1] - 1
+            vim.api.nvim_buf_set_lines(0, row, row, false, { date, "" })
+          end,
+          -- the rest are options for creating user commands (:h nvim_create_user_command)
+          desc = "Reload AstroNvim (Experimental)",
+        },
+      }
+
       local maps = opts.mappings
 
       -- Markdown mappings

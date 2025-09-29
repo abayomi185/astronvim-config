632827082b25d0e4aa6945c5109b162699e02f37
Yomi Ikuru <captyomjnr@gmail.com>
2025-09-29 10:47:04 +0000

    feat(core): manually set clipboard command to win32yank
    Issue started because neovim checks for $WAYLAND_DISPLAY which seems to
    be set for my WSL NixOS. Not sure if this is a new change in neovim or
    my NixOS config but setting this manually works ok. I don't particularly
    need osc52 on this setup

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -39,6 +39,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        clipboard = "win32yank",
       },
       o = {
         hls = true, -- set highlight search
